ebdf8094482b2b021e6f6eb073f99ff993e79330
Soen Dayoung <kkbo5222@gmail.com>
2021-03-10 15:19:59 +0900

    Edit Static Files

--- TimeBank_account/urls.py
+++ TimeBank_account/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register),
-    path('login/', views.login)
+    path('login/', views.login),
 ]
--- TimeBank_account/views.py
+++ TimeBank_account/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def register(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+        auth.login(request, user)
+        return redirect('index')
     return render(request, 'register.html')
 
 def login(request):
     return render(request, 'login.html')
+
+def logout(request):
+    pass

209ada4c601073baf4e8e91a07434742296c5123
kkbo5222 <kkbo5222@gmail.com>
2021-10-02 19:41:43 +0900

    [chore] update Category Admin
    - DraggableMPTTADmin 사용
    : drag&drop으로 트리 구조 위치 조정

--- TimeBank_app/admin.py
+++ TimeBank_app/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Post, Comment, Apply, Review, ReComment, Category
+from mptt.admin import DraggableMPTTAdmin
+
+
+
+# 드래그앤드롭으로 트리 구조 위치 조정
+class CategoryAdmin(DraggableMPTTAdmin):
+    # 모델당 픽셀 레벨 지정
+    mptt_level_indent = 20
+    # 들여쓰기할 필드 지정
+    # mptt_indent_field = "some_node_field"
+
 
 admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Comment)
 admin.site.register(ReComment)
 admin.site.register(Apply)
